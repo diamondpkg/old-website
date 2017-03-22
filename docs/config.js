04666937a5f474386283364529c5d91bd10f7c2b
Zachary <admin@hackzzila.com>
2017-03-21 21:51:36 -0400

    Docs on install

--- docs/config.js
+++ docs/config.js
@@ -1,4 +1,8 @@
 self.$config = {
   home: 'https://raw.githubusercontent.com/diamondpkg/diamond/master/README.md',
   repo: 'diamondpkg/diamond',
+  nav: [
+    {title: 'Home', path: '/'},
+    {title: 'Install', path: '/install'}
+  ]
 }

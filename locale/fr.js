857ae432ad7173476089ebae0a3f3d5ea96d97b1
Darqam <anhim2@gmail.com>
2017-04-29 08:51:51 -0600

    Adding first version of fr locale
    Creation of fr.js for i18n use on the website

---
+++ locale/fr.js
@@ -0,0 +1,7 @@
+if (!window.locale) window.locale = {};
+
+window.locale.en = {
+  "home": "Acceuil",
+  "docs": "Documentation",
+  "main": "Gestionnaire de dépendences construit pour Sass, Less, and CSS.",
+};
